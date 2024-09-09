0efc7b5128b37d969a37a2c12a7924c723523315
Dmytro Honcharov <dmytro.honch@gmail.com>
2024-09-09 18:24:55 -0500

    add counts

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
 const container = document.getElementById('app');
 const daysContainer = document.createElement('div');
 const timeContainer = document.createElement('div');
+const emojiContainer = document.createElement('div');
+
 container.appendChild(daysContainer);
 container.appendChild(timeContainer);
+container.appendChild(emojiContainer);
+
+emojiContainer.classList.add('emoji');
+
 const targetDateInUTC = new Date("2024-09-22T11:30:00Z");
+
+const heartArray = ["♡", "♥"];
 
 function updateCountdown() {
   const now = new Date();
@@ -21,6 +29,17 @@ function updateCountdown() {
 
   daysContainer.textContent = days < 1 ? '' : `${String(days)} days`;
   timeContainer.textContent = `${String(hours).padStart(2, '0')}:${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')}`;
+
+  let str = "";
+  for (let i = 0; i < 60; i++) {
+    str += " ";
+    if (i <= seconds) {
+      str += heartArray[1];
+    } else {
+      str += "♡";
+    }
+  }
+  emojiContainer.textContent = str;
 
   setTimeout(() => {
     requestAnimationFrame(updateCountdown);
